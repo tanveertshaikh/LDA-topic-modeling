43c1905e94cde6079c7ba72d24adc7b83223daba
Tanveer Shaikh <tshaik4@uic.edu>
2018-05-04 03:08:58 -0500

    Update .py file and bash script

--- executable/run.sh
+++ executable/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+pip3 install gensim
 pip3 install numpy
 pip3 install pandas
+pip3 install pyLDAvis
 pip3 install nltk
 pip3 install scipy
 pip3 install spacy
-pip3 install stanfordcorenlp
 pip3 install sklearn
-pip3 install gensim
+pip3 install tqdm
 
-python3 ../src/evaluate.py
+python3 ../src/topic_modeling.py
