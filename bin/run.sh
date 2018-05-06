f3c6189f47e26428bc9d4bb48cd091a9cc335320
Tanveer Shaikh <tshaik4@uic.edu>
2018-05-06 02:48:56 -0500

    Added learning curves

--- bin/run.sh
+++ bin/run.sh
@@ -5,8 +5,10 @@ pip3 install IPython
 pip3 install matplotlib
 pip3 install numpy
 pip3 install pandas
+pip3 install pickle
 pip3 install pyldavis
 pip3 install nltk
+pip3 install seaborn
 pip3 install scipy
 pip3 install spacy
 pip3 install sklearn
