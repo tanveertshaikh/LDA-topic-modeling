95ee88910c03b2a1a611ecd07c2211393ed92054
Tanveer Shaikh <tshaik4@uic.edu>
2018-05-05 22:26:27 -0500

    Working LDA model and cross-validated

--- bin/run.sh
+++ bin/run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 pip3 install gensim
-pip3 install ipython
+pip3 install IPython
 pip3 install matplotlib
 pip3 install numpy
 pip3 install pandas
-pip3 install pyLDAvis
+pip3 install pyldavis
 pip3 install nltk
 pip3 install scipy
 pip3 install spacy
 pip3 install sklearn
 pip3 install tqdm
+
+spacy -m downlaod en
 
 python3 ../src/topic_modeling.py
